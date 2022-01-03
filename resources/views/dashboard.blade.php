35377c114907aa540ef4fd7868abd2e04cf14c7f
Anna <annavidaltur@gmail.com>
2022-01-03 17:05:06 +0100

    jetstream navbar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,18 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+        <h2 class="font-semibold text-xl text-blue-800 leading-tight">
+            Blog
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, quae beatae rerum maxime eum omnis, distinctio alias aliquid eveniet ad inventore neque, sapiente nesciunt animi aperiam modi officia nihil ex.</p>
+                <p>Illo illum possimus id dolore impedit placeat voluptatem non doloremque minus quos adipisci veniam sunt nemo temporibus excepturi, minima, blanditiis debitis recusandae soluta? Eum iusto similique vero? Dignissimos, cum cumque?</p>
+                <p>Hic omnis quibusdam iste? Non, voluptatibus minima. Quos reprehenderit veniam eum incidunt vero nihil commodi tempora iusto suscipit debitis corrupti doloremque beatae, pariatur odio, deserunt dolorum recusandae nostrum quas vel.</p>
+                <p>Adipisci, ut tenetur. Esse libero illo atque quod. Officia aliquam perferendis distinctio hic harum magnam dolor, eveniet magni laboriosam. Impedit facilis molestias alias, accusantium sequi labore nemo omnis similique culpa!</p>
+                <p>Consequuntur hic quidem iure inventore dolorem eum quaerat quia voluptate quam cum tenetur accusantium aperiam totam natus porro quae explicabo doloribus architecto id, nam animi reiciendis officiis. Nobis, aliquam ex.</p>
             </div>
         </div>
     </div>
